0a36e4f2990b5b265777fd5ff7120a12165adaa7
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-02-27 12:08:23 -0600

    Update main.py
    Code solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
-"""
+
 
 # Declaraciones
 CONSTANTE = valor
@@ -13,3 +13,37 @@ entrada = input()
 
 # Salidas
 print(salida)
+
+"""
+
+"""
+Calcular el importe de ISR correspondiente a un sueldo mensual
+"""
+
+# Entradas
+sueldo = eval(input("Introduzca el sueldo mensual: "))
+	
+# Proceso
+if sueldo <= 3644.94:
+	lim_inf = 0.01
+	c_fija = 12.88
+	porc_exc = 0.10
+elif sueldo <= 7446.19:
+	lim_inf = 3644.95
+	c_fija = 303.76
+	porc_exc = 0.20
+elif sueldo <= 10298.35:
+	lim_inf = 7446.20
+	c_fija = 1063.92
+	porc_exc = 0.30
+else:
+	lim_inf = 10298.36
+	c_fija = 3327.42
+	porc_exc = 0.35
+
+excedente = sueldo - lim_inf
+marginal = excedente * porc_exc
+isr = c_fija + marginal
+	
+# Salidas
+print("El ISR es:", isr)
