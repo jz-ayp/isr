b5a8888da5e7817332f29ed1df1bfca51574db6b
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2023-10-06 00:42:12 -0600

    Add solution

--- isr.py
+++ isr.py
@@ -1,15 +1,32 @@
+# -*- coding: utf-8 -*-
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Calcular el importe de ISR correspondiente a un sueldo mensual
 """
-
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
-
+sueldo = eval(input("Introduzca el sueldo mensual: "))
+	
 # Proceso
+if sueldo <= 3644.94:
+	lim_inf = 0.01
+	c_fija = 12.88
+	porc_exc = 0.10
+elif sueldo <= 7446.19:
+	lim_inf = 3644.95
+	c_fija = 303.76
+	porc_exc = 0.20
+elif sueldo <= 10298.35:
+	lim_inf = 7446.20
+	c_fija = 1063.92
+	porc_exc = 0.30
+else:
+	lim_inf = 10298.36
+	c_fija = 3327.42
+	porc_exc = 0.35
 
-
+excedente = sueldo - lim_inf
+marginal = excedente * porc_exc
+isr = c_fija + marginal
+	
 # Salidas
-print(salida)
+print("El ISR es:", isr)
